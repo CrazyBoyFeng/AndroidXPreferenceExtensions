8dbde61c85a07aeb8e0476a4426625601afe9f15
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-29 00:19:22 +0800

    Update demo

--- demo/src/main/kotlin/crazyboyfeng/android/preference/demo/SettingsFragment.kt
+++ demo/src/main/kotlin/crazyboyfeng/android/preference/demo/SettingsFragment.kt
@@ -1,16 +1,21 @@
 package crazyboyfeng.android.preference.demo
 
 import android.os.Bundle
+import androidx.preference.EditTextPreferencePlus
 import androidx.preference.NumberPickerPreference
 import crazyboyfeng.android.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_preferences, rootKey)
+
         val numberPickerPreference = findPreference<NumberPickerPreference>("numberpicker")
         numberPickerPreference?.minValue = 1
         numberPickerPreference?.maxValue = 15
         numberPickerPreference?.wrapSelectorWheel = true
         numberPickerPreference?.formatSummary = true
+
+        val editTextPreferencePlus = findPreference<EditTextPreferencePlus>("edittext")
+        editTextPreferencePlus?.formatSummary = true
     }
 }
