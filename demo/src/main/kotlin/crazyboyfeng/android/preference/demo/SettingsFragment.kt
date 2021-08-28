11baf6084e63dd38b959f22b1958260774faabb6
CrazyBoyFeng <CrazyBoyFeng@Live.Com>
2021-08-28 15:52:30 +0800

    Update demo

--- demo/src/main/kotlin/crazyboyfeng/android/preference/demo/SettingsFragment.kt
+++ demo/src/main/kotlin/crazyboyfeng/android/preference/demo/SettingsFragment.kt
@@ -7,6 +7,9 @@ import crazyboyfeng.android.preference.PreferenceFragmentCompat
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings_preferences, rootKey)
-        val numberPickerPreference=findPreference<NumberPickerPreference>("numberpicker")
+        val numberPickerPreference = findPreference<NumberPickerPreference>("numberpicker")
+        numberPickerPreference?.minValue = 1
+        numberPickerPreference?.maxValue = 15
+        numberPickerPreference?.wrapSelectorWheel = true
     }
 }
